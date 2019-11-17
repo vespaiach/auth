d224550a5d34531340f9d52b4602409c20fe825b
Toan Nguyen <nta.toan@gmail.com>
2019-11-17 11:17:01 +0700

    Seeding database and adding sample app

--- pkg/storage/mysql/db.go
+++ pkg/storage/mysql/db.go
@@ -8,117 +8,6 @@ import (
 	"sync"
 )
 
-var initDatabase = `
-CREATE TABLE IF NOT EXISTS "keys" (
-  	"id" BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-	"key" VARCHAR(32) NOT NULL DEFAULT '',
-	"desc" VARCHAR(64) NOT NULL DEFAULT '',
-  	"created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  	"updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY ("id"),
-  UNIQUE INDEX "keys_key_uniq" ("key" ASC))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS "bunches" (
-  "id" BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  "name" VARCHAR(32) NOT NULL DEFAULT '',
-  "desc" VARCHAR(64) NOT NULL DEFAULT '',
-  "active" TINYINT(1) UNSIGNED NOT NULL DEFAULT 1,
-  "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY ("id"),
-  UNIQUE INDEX "bunch_name_uniq" ("name" ASC),
-  INDEX "bunch_active_idx" ("active" ASC))
-ENGINE = InnoDB;
-
-CREATE TABLE IF NOT EXISTS "users" (
-  "id" BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  "username" VARCHAR(32) NOT NULL,
-  "email" VARCHAR(64) NOT NULL,
-  "hash" VARCHAR(255) NOT NULL,
-  "active" TINYINT(1) NOT NULL DEFAULT 1,
-  "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  "updated_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY ("id"),
-  UNIQUE INDEX "users_username_uniq" ("username" ASC),
-  UNIQUE INDEX "users_email_uniq" ("email" ASC),
-  INDEX "users_active_idx" ("active" ASC))
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS "token_histories" (
-  "uid" VARCHAR(36) NOT NULL,
-  "user_id" BIGINT(20) UNSIGNED NOT NULL,
-  "access_token" VARCHAR(1024) NOT NULL,
-  "refresh_token" VARCHAR(1024) NOT NULL DEFAULT '',
-  "remote_addr" VARCHAR(512) NOT NULL DEFAULT '',
-  "x_forwarded_for" VARCHAR(512) NOT NULL DEFAULT '',
-  "x_real_ip" VARCHAR(512) NOT NULL DEFAULT '',
-  "user_agent" VARCHAR(512) NOT NULL DEFAULT '',
-  "created_at" TIMESTAMP NOT NULL,
-  "expired_at" TIMESTAMP NOT NULL,
-  PRIMARY KEY ("uid"),
-  UNIQUE INDEX "uid_uniq" ("uid" ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS "bunch_keys" (
-  "id" BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  "bunch_id" BIGINT(20) UNSIGNED NOT NULL,
-  "key_id" BIGINT(20) UNSIGNED NOT NULL,
-  "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY ("id"),
-  INDEX "bunch_key_key_id_idx" ("key_id" ASC),
-  INDEX "bunch_key_bunch_id_idx" ("bunch_id" ASC),
-  UNIQUE INDEX "bunch_key_uniq" ("bunch_id" ASC, "key_id" ASC),
-  CONSTRAINT "key_id_on_bunch_key"
-    FOREIGN KEY ("key_id")
-    REFERENCES "keys" ("id")
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT "role_id_on_bunch_key"
-    FOREIGN KEY ("bunch_id")
-    REFERENCES "bunches" ("id")
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8;
-
-CREATE TABLE IF NOT EXISTS "user_bunches" (
-  "id" BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  "user_id" BIGINT(20) UNSIGNED NOT NULL,
-  "bunch_id" BIGINT(20) UNSIGNED NOT NULL,
-  "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY ("id"),
-  INDEX "user_bunch_user_id_idx" ("user_id" ASC),
-  INDEX "user_bunch_bunch_id_idx" ("bunch_id" ASC),
-  UNIQUE INDEX "user_bunch_uniq" ("user_id" ASC, "bunch_id" ASC),
-  CONSTRAINT "user_id_on_user_bunch"
-    FOREIGN KEY ("user_id")
-    REFERENCES "users" ("id")
-    ON DELETE CASCADE
-    ON UPDATE CASCADE,
-  CONSTRAINT "bunch_id_on_user_bunch"
-    FOREIGN KEY ("bunch_id")
-    REFERENCES "bunches" ("id")
-    ON DELETE CASCADE
-    ON UPDATE CASCADE)
-ENGINE = InnoDB;
-`
-
-var dropDatabase = `
-DROP TABLE IF EXISTS "user_bunches";
-DROP TABLE IF EXISTS "bunch_keys";
-DROP TABLE IF EXISTS "keys";
-DROP TABLE IF EXISTS "bunches";
-DROP TABLE IF EXISTS "users";
-DROP TABLE IF EXISTS "token_histories";
-`
-
 type uniqInt struct {
 	order int
 	mux   sync.Mutex
@@ -149,6 +38,7 @@ type Migrator struct {
 	db   *sqlx.DB
 	init []*Script
 	drop []*Script
+	seed []*Script
 }
 
 // NewMigrator return struct instance
@@ -162,10 +52,15 @@ func NewMigrator(db *sqlx.DB) *Migrator {
 		&Script{Name: "drop_database", Text: dropDatabase},
 	}
 
+	var seedScripts = []*Script{
+		&Script{Name: "seed_database", Text: seedingData},
+	}
+
 	return &Migrator{
 		db,
 		initScripts,
 		dropScripts,
+		seedScripts,
 	}
 }
 
@@ -186,6 +81,17 @@ func (m *Migrator) Drop() {
 
 	for i := len(m.drop) - 1; i >= 0; i-- {
 		tx.MustExec(santizeSQL(m.drop[i].Text))
+	}
+
+	tx.Commit()
+}
+
+// Seed database
+func (m *Migrator) Seed() {
+	tx := m.db.MustBegin()
+
+	for i := len(m.seed) - 1; i >= 0; i-- {
+		tx.MustExec(santizeSQL(m.seed[i].Text))
 	}
 
 	tx.Commit()
